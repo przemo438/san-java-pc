c4e7469dac564ec19e3020e8be7571aa268daf2b
przemo438 <przemo438@gmail.com>
2013-04-28 12:43:06 +0200

    Zadanie 3 - GRA (finally)

--- zad3/src/gra/Gra.java
+++ zad3/src/gra/Gra.java
@@ -1,4 +1,5 @@
 package gra;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,44 +7,38 @@ import java.util.Random;
 
 public class Gra {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
 
-				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-				boolean wynik = false;
-				int ktory_raz=1;
-		        Random rand = new Random();
-		        int liczba = rand.nextInt(100);
-		        while (wynik==false)
-				{
-		        	System.out.print("Wpisz liczbê z zakresu 0-100: ");
-		        	try {
-		        	int zgadnij = Integer.parseInt(reader.readLine());
-		        	if(zgadnij==liczba)
-					{
-		        		System.out.println("");
-		        		System.out.println("Sukces!!!");
-		        		System.out.println("Znalaz³eœ szukan¹ liczbê!");
-		        		wynik=true;
-		        	}
-		        	else if (zgadnij>liczba)
-					{
-		        		System.out.println("Poda³eœ za du¿¹ liczbê.");
-		        		wynik=false;
-		        		ktory_raz++;
-		        	}
-		        	else if (zgadnij<liczba)
-					{
-		        		System.out.println("Poda³eœ za ma³¹ liczbê.");
-		        		wynik=false;
-		        		ktory_raz++;
-		        	}
-		        	} 
-		        	catch (NumberFormatException e){
-		        	System.out.println("Podany znak nie jest liczb¹!");
-		        	}	
-		        }
-		        System.out.println("Uda³o siê za "+ktory_raz+" razem.");
-	}	
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		boolean wynik = false;
+		int ktory_raz = 1;
+		Random rand = new Random();
+		int liczba = rand.nextInt(100);
+		while (wynik == false) {
+			System.out.print("Wpisz liczbê z zakresu 0-100: ");
+			try {
+				int zgadnij = Integer.parseInt(reader.readLine());
+				if (zgadnij == liczba) {
+					System.out.println("");
+					System.out.println("Sukces!!!");
+					System.out.println("Znalaz³eœ szukan¹ liczbê!");
+					wynik = true;
+				} else if (zgadnij > liczba) {
+					System.out.println("Poda³eœ za du¿¹ liczbê.");
+					wynik = false;
+					ktory_raz++;
+				} else if (zgadnij < liczba) {
+					System.out.println("Poda³eœ za ma³¹ liczbê.");
+					wynik = false;
+					ktory_raz++;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Podany znak nie jest liczb¹!");
+			}
+		}
+		System.out.println("Uda³o siê za " + ktory_raz + " razem.");
+	}
 
 }
